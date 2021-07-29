6368a19a66df2e3dbc8fb346ef9099316de1f36b
blue <blue.yuan@orderplus.com>
2021-07-29 13:47:12 +0800

    2021.7.29

---
+++ 20210729/tools.js
@@ -0,0 +1,40 @@
+// 封装call 方法封装
+function call(Fn, obj, ...params) {
+    //调用对象为null 或者不存在的时候 调用对象指向全局对象
+    if (obj === 'underfine' || obj === null) {
+        obj = globalThis; // node 、window
+    }
+    obj.hander = Fn;
+    let result = obj.hander(...params);
+    delete obj.hander;
+    return result;
+}
+// 封装apply 方法
+function apply(Fn, obj, args) {
+    if (obj === 'underfine' || obj === null) {
+        obj = globalThis;
+    }
+    obj.hander = Fn;
+    let result = obj.hander(...args);
+    delete obj.hander;
+    return result;
+}
+// 封装 bind方法
+function bind(Fn, obj, ...args) {
+    return function (...args1) {
+        return call(Fn, obj, ...args, ...args1)
+    }
+}
+// 封装节流方法
+function throttle(callback, wait){
+    // 起始时间
+    let start = 0;
+    return function(e){
+        // 终止时间
+        let now = Date.now();
+        if(now-start>wait){
+            callback.call(this,e);
+            start = now;
+        }
+    }
+}
