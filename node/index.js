7eeb89b16b071bda07b6e93a92d7f8b40f935550
blue <blue.yuan@orderplus.com>
2021-08-24 14:20:46 +0800

    code

--- node/index.js
+++ node/index.js
@@ -1,5 +1,15 @@
-var http = require('http');
-http.createServer(function(req,res){
-    res.write('<head><meta charst = "utf-8"/></head>');
-    res.write("hello")
-}).listen(3000)
+//引入http模块
+var http = require("http");
+//设置主机名
+var hostName = '110.42.129.142';
+//设置端口
+var port = 443;
+//创建服务
+var server = http.createServer(function(req,res){
+    res.setHeader('Content-Type','text/plain');
+    res.end("hello nodejs");
+
+});
+server.listen(port,hostName,function(){
+    console.log(`服务器运行在http://${hostName}:${port}`);
+});
